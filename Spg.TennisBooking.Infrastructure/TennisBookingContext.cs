8ee5969a2995e6405241502a8bc39335ddf3d4f6
LuckyForce <adrian.schauer@aon.at>
2022-09-27 15:56:17 +0200

    First DbContext

---
+++ Spg.TennisBooking.Infrastructure/TennisBookingContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Spg.TennisBooking.Infrastructure
+{
+    public class TennisBookingContext : DbContext
+    {
+        protected TennisBookingContext() : this(new DbContextOptions<DbContext>())
+        {
+        }
+
+        public TennisBookingContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
