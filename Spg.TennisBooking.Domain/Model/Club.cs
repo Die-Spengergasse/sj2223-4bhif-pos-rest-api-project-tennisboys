93579039e7841f4ae9a8d27446a022f31b21dea9
LuckyForce <adrian.schauer@aon.at>
2022-10-05 21:51:01 +0200

    First working tests

--- Spg.TennisBooking.Domain/Model/Club.cs
+++ Spg.TennisBooking.Domain/Model/Club.cs
@@ -12,7 +12,17 @@ namespace Spg.TennisBooking.Domain.Model
         public string ClubName { get; set; } = string.Empty;
         public string ClubInfo { get; set; } = string.Empty;
         public string ImagePath { get; set; } = string.Empty;
-        
+        public SocialHub SocialHub { get; } = new SocialHub();
 
+        private List<Court> _courts = new();
+        public IReadOnlyList<Court> Courts => _courts;
+
+        public void AddCourt(Court entity)
+        {
+            if (entity is not null)
+            {
+                _courts.Add(entity); // UnitTest
+            }
+        }
     }
 }
--- Spg.TennisBooking.Domain/Model/SocialHub.cs
+++ Spg.TennisBooking.Domain/Model/SocialHub.cs
@@ -9,13 +9,30 @@ namespace Spg.TennisBooking.Domain.Model
     public class SocialHub
     {
         public int Id { get; set; }
-        public string Facebook { get; set; } = string.Empty;
+        private string _facebook { get; set; } = string.Empty;
+        public string Facebook
+        {
+            get => _facebook;
+        }
+
         public string Instagram { get; set; } = string.Empty;
         public string Twitter { get; set; } = string.Empty;
         public string Youtube { get; set; } = string.Empty;
         public string LinkedIn { get; set; } = string.Empty;
-        public int Telephone { get; set; }
+        public string Telephone { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Website { get; set; } = string.Empty;
+
+        public SocialHub()
+        {
+            
+        }
+
+        //TODO: Set Methods for all properties
+
+        public void ChangeFacebook(string content)
+        {
+            _facebook = content;
+        }
     }
 }
