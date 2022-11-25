8690b3aeba9272fe70fc847ac2ee615406cecfbf
LuckyForce <adrian.schauer@aon.at>
2022-11-25 14:51:11 +0100

    Launch Fix

--- Spg.TennisBooking.MvcFrontEnd/Controllers/HomeController.cs
+++ Spg.TennisBooking.MvcFrontEnd/Controllers/HomeController.cs
@@ -1,32 +1,32 @@
-﻿// using Microsoft.AspNetCore.Mvc;
-// using Spg.TennisBooking.MvcFrontEnd.Models;
-// using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
+using Spg.TennisBooking.MvcFrontEnd.Models;
+using System.Diagnostics;
 
-// namespace Spg.TennisBooking.MvcFrontEnd.Controllers
-// {
-//     public class HomeController : Controller
-//     {
-//         private readonly ILogger<HomeController> _logger;
+namespace Spg.TennisBooking.MvcFrontEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
 
-//         public HomeController(ILogger<HomeController> logger)
-//         {
-//             _logger = logger;
-//         }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-//         public IActionResult Index()
-//         {
-//             return View();
-//         }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-//         public IActionResult Privacy()
-//         {
-//             return View();
-//         }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-//         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-//         public IActionResult Error()
-//         {
-//             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-//         }
-//     }
-// }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
