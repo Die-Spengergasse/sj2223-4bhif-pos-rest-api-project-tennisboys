bfa08ca41a2a31d5592c1b7bed77a5bda2050215
Flayser <las20298@spengergasse.at>
2022-12-30 14:32:46 +0100

    Created Dto constructors

--- src/Spg.TennisBooking.Api/Dtos/AuthDtos/VerifyDto.cs
+++ src/Spg.TennisBooking.Api/Dtos/AuthDtos/VerifyDto.cs
@@ -4,5 +4,12 @@ namespace Spg.TennisBooking.Api.Dtos.AuthDtos
     {
         public string UUID { get; set; } = string.Empty;
         public string VerificationCode { get; set; } = string.Empty;
+
+        //Constructor
+        public VerifyDto(string uuid, string verificationCode)
+        {
+            UUID = uuid;
+            VerificationCode = verificationCode;
+        }
     }
 }
