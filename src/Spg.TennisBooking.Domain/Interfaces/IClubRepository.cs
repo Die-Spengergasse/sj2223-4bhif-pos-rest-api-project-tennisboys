d6ba67d36bf9391341a6c46d025bb86c96c7c3bc
LuckyForce <adrian.schauer@aon.at>
2023-01-03 02:18:21 +0100

    Club Repository Implementation

--- src/Spg.TennisBooking.Domain/Interfaces/IClubRepository.cs
+++ src/Spg.TennisBooking.Domain/Interfaces/IClubRepository.cs
@@ -3,6 +3,10 @@ using Spg.TennisBooking.Domain.Model;
 namespace Spg.TennisBooking.Domain.Interfaces
 {
     public interface IClubRepository
-    {   
+    {
+        Club? GetById(int id);
+        Club? GetByLink(string link);
+        bool Update(Club club);
+        bool Delete(Club club);
     }
 }
