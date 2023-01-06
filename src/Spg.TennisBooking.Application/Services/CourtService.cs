ed31f9682ad885d85916de899af39f8a6856ba58
LuckyForce <adrian.schauer@aon.at>
2023-01-06 18:51:45 +0100

    Service Interface

--- src/Spg.TennisBooking.Application/Services/CourtService.cs
+++ src/Spg.TennisBooking.Application/Services/CourtService.cs
@@ -8,16 +8,47 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Security.Cryptography;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Spg.TennisBooking.Application.Services
 {
     public class CourtService : ICourtService
     {
         private readonly ICourtRepository _courtRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly ILogger<CourtService> _logger;
 
-        public CourtService(ICourtRepository courtRepository)
+        public CourtService(ICourtRepository courtRepository, IUserRepository userRepository, ILogger<CourtService> logger)
         {
             _courtRepository = courtRepository;
+            _userRepository = userRepository;
+            _logger = logger;
+        }
+
+        public Task<IActionResult> Delete(int id, string uuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> GetAll(string clubLink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Patch(Court court, string uuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Post(Court court, string uuid)
+        {
+            throw new NotImplementedException();
         }
     }
 }
