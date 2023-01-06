64a06e968c191df04b746bc831a7a423161169dd
Flayser <las20298@spengergasse.at>
2023-01-06 20:39:05 +0100

    Created UserServiceTests

--- src/Spg.TennisBooking.Application.Test/Services/UserServiceTests.cs
+++ src/Spg.TennisBooking.Application.Test/Services/UserServiceTests.cs
@@ -4,6 +4,7 @@ using Spg.TennisBooking.Domain.Interfaces;
 using Spg.TennisBooking.Domain.Model;
 using Spg.TennisBooking.Infrastructure;
 using Spg.TennisBooking.Repository.Repositories;
+using System;
 using System.Net;
 using System.Security.Cryptography;
 using Xunit;
@@ -50,21 +51,72 @@ namespace Spg.TennisBooking.Application.Test.Services
         [Fact]
         public void GetPersonalData()
         {
+            //Init
+            TennisBookingContext context = GetContext();
+            UserService userService = GetService(GetRepository(context));
+
+            //Arrange
+            string UUID = "00000000-0000-0000-0000-000000000000";
+
+            //Act
+            User result = userService.GetPersonalData(UUID);
+
+            //Assert
             Assert.True(true);
+
+            //Clean
+            context.Database.EnsureDeleted();
+
+
         }
 
         //SetPersonalData
         [Fact]
         public void SetPersonalData()
         {
+            //Init
+            TennisBookingContext context = GetContext();
+            UserService userService = GetService(GetRepository(context));
+
+            //Arrange
+            string UUID = "00000000-0000-0000-0000-000000000000";
+            string firstName = "Adrian";
+            string lastName = "Schauer";
+            DateTime? birthDate = new DateTime(1989, 12, 12);
+            GenderTypes gender = GenderTypes.Male;
+            PhoneNumber? phoneNumber = new PhoneNumber("+43", "6641234567");
+
+            //Act
+            bool result = userService.SetPersonalData(UUID, firstName, lastName, birthDate, gender, phoneNumber);
+
+            //Assert
             Assert.True(true);
+
+            //Clean
+            context.Database.EnsureDeleted();
         }
 
         //ChangePassword
         [Fact]
         public void ChangePassword()
         {
+            //Init
+            TennisBookingContext context = GetContext();
+            UserService userService = GetService(GetRepository(context));
+
+            //Arrange
+            string UUID = "00000000-0000-0000-0000-000000000000";
+            string Password = "admin1234";
+            string newPassword = "admin12345";
+
+            //Act
+            bool result = userService.ChangePassword(UUID, Password, newPassword);
+
+            //Assert
             Assert.True(true);
+
+            //Clean
+            context.Database.EnsureDeleted();
         }
     }
 }
