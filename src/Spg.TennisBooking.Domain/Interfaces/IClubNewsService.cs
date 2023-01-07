c601d2e8fe296a0a48b9329744a5e6a13fece98f
LuckyForce <adrian.schauer@aon.at>
2023-01-07 18:15:30 +0100

    IClubNewsService

--- src/Spg.TennisBooking.Domain/Interfaces/IClubNewsService.cs
+++ src/Spg.TennisBooking.Domain/Interfaces/IClubNewsService.cs
@@ -1,8 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Spg.TennisBooking.Domain.Dtos.ClubNewsDtos;
 using Spg.TennisBooking.Domain.Model;
 
 namespace Spg.TennisBooking.Domain.Interfaces
 {
     public interface IClubNewsService
     {
+        /* ClubEvent:
+         * Get(id)
+         * GetAll(Club Link)
+         * Post
+         * Put(id)
+         * Delete(id)
+         */
+
+        Task<IActionResult> Get(int id);
+        Task<IActionResult> GetAll(string clubLink);
+        Task<IActionResult> Post(PostClubNewsDto postClubEventDto, string uuid);
+        Task<IActionResult> Put(PutClubNewsDto putClubEventDto, string uuid);
+        Task<IActionResult> Delete(int id, string uuid);
     }
 }
