c3ebb6823d20e063bf4f8a9d3e10b5e502deeef1
LuckyForce <adrian.schauer@aon.at>
2023-01-08 09:39:38 +0100

    IReservationService

--- src/Spg.TennisBooking.Domain/Interfaces/IReservationService.cs
+++ src/Spg.TennisBooking.Domain/Interfaces/IReservationService.cs
@@ -1,8 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Spg.TennisBooking.Domain.Dtos.ReservationDtos;
 using Spg.TennisBooking.Domain.Model;
 
 namespace Spg.TennisBooking.Domain.Interfaces
 {
     public interface IReservationService
     {
+        /*
+         * GetReservationsByCourtId - Only When (Dto as Reponse)
+           GetReservationById - Admin
+           PostReservation
+           DeleteReservation - Admin
+           GetReservationsByUser - Only Themself
+           GetReservationsByClub - Admin
+         */
+        Task<IActionResult> GetByCourt(int courtId);
+        Task<IActionResult> GetById(int id, string uuid);
+        Task<IActionResult> Post(PostReservationDto reservation, string uuid);
+        Task<IActionResult> Delete(int id, string uuid);
+        Task<IActionResult> GetByUser(string uuid);
+        Task<IActionResult> GetByClub(string clubLink, string uuid);
     }
 }
